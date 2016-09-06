f6ec775b34db27fe6f4c27944f6da788298497cf
Aki <mifukia1101@gmail.com>
2016-09-06 23:54:44 +0900

    added mypeg and testjs

---
+++ test.js
@@ -0,0 +1,13 @@
+PEG = require('pegjs');
+var assert = require('assert');
+var fs = require('fs'); // for loading files
+
+// Read file contents
+var data = fs.readFileSync('my.peg', 'utf-8');
+// Show the PEG grammar file
+console.log(data);
+// Create my parser
+var parse = PEG.generate(data).parse;
+// Do a test
+assert.deepEqual( parse("(a b c)"), ["a", "b", "c"] );
+console.log(parse("(a b c)"));
